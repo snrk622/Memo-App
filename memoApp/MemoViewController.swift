91da1453422cad9034a20e02a3428d3acb222b4f
snrk622 <s.0622.r@icloud.com>
2018-10-31 17:39:59 +0900

    first commit

---
+++ memoApp/MemoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MemoViewController.swift
+//  memoApp
+//
+//  Created by 篠原立樹 on 2018/10/31.
+//  Copyright © 2018 Ostrich. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+    
+    var memo: String?
+
+    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
